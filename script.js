9bf8fc3a69afd4143d0de10347cd5d43d12b5a8f
Patricia Fiol <patricia.fiol@outlook.com>
2023-01-30 10:02:41 +0100

    add faq cards and refactor

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// Circle toggle navigation
+
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+// Faq toggle questions
+
+const toggleBtn = document.querySelectorAll('.faq-show')
+
+toggleBtn.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
